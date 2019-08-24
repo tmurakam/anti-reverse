04de9f861514741227122ffd6cd1af7fb3a176ca
Takuya Murakami <murakami@da.jp.nec.com>
2019-08-24 16:24:36 +0900

    add detectDebugger()

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <sys/ptrace.h>
+
+bool detectDebugger() {
+  if (ptrace(PT_TRACE_ME, 0, 0, 0) < 0) {
+    printf("Runs under debugger..\n");
+    return true;
+  }
+  return false;
+}
 
 int main() {
+  detectDebugger();
+
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }
