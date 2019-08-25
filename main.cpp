faf10919ac48922abc7383bc20146ee46bc70a46
Takuya Murakami <tmurakam@tmurakam.org>
2019-08-25 15:32:53 +0900

    fork test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <sys/ptrace.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 bool detectDebugger() {
   if (ptrace(PT_TRACE_ME, 0, 0, 0) < 0) {
@@ -12,6 +14,15 @@ bool detectDebugger() {
 int main() {
   detectDebugger();
 
-  std::cout << "Hello, World!" << std::endl;
+  pid_t child = fork();
+  if (child > 0) {
+    std::cout << "I am parent, child pid = " << child << std::endl;
+  } else {
+    detectDebugger();
+    std::cout << "I am child." << std::endl;
+  }
+  while (true) {
+    sleep(100);
+  }
   return 0;
 }
