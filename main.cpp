9647d0506a67aca0da94a7eed72582befbcc9dae
Takuya Murakami <tmurakam@tmurakam.org>
2019-08-25 15:38:43 +0900

    fix indent 4

--- main.cpp
+++ main.cpp
@@ -3,26 +3,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-bool detectDebugger() {
-  if (ptrace(PT_TRACE_ME, 0, 0, 0) < 0) {
-    printf("Runs under debugger..\n");
-    return true;
-  }
-  return false;
+bool blockDebugger() {
+    if (ptrace(PT_TRACE_ME, 0, 0, 0) < 0) {
+        printf("Runs under debugger..\n");
+        return true;
+        //abort();
+    } else {
+        printf("Not runs under debugger..\n");
+    }
+    return false;
 }
 
 int main() {
-  detectDebugger();
+    blockDebugger();
 
-  pid_t child = fork();
-  if (child > 0) {
-    std::cout << "I am parent, child pid = " << child << std::endl;
-  } else {
-    detectDebugger();
-    std::cout << "I am child." << std::endl;
-  }
-  while (true) {
-    sleep(100);
-  }
-  return 0;
+    pid_t child = fork();
+    if (child > 0) {
+        std::cout << "I am parent, parent pid = " << getpid() << ", child pid = " << child << std::endl;
+    } else {
+        blockDebugger();
+        std::cout << "I am child." << std::endl;
+    }
+
+    while (true) {
+        sleep(100);
+    }
+    return 0;
 }
